2707a22470d5d7eb338a4f85569e44130735cdc2
Sarasarita2017 <hussam090717@gmail.com>
2023-07-08 15:26:40 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Prints the multiplication of two args numbers
+ * main - Prints the sum of args positive numbers
  * @argc: argument count
  * @argv: argument vector
  *
@@ -11,18 +12,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1 = 0, n2 = 0;
+	int i;
+	unsigned int k, sum = 0;
+	char *e;
 
-	if (argc == 3)
+	if (argc > 1)
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
+		for (i = 1; i < argc; i++)
+		{
+			e = argv[i];
+
+			for (k = 0; k < strlen(e); k++)
+			{
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(e);
+			e++;
+		}
+		printf("%d\n", sum);
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
 	}
 
 	return (0);
